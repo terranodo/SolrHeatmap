ed0fe47f8b0585b2783cc63fa35f9109f487aaa9
Lenninlasd <lenninlasd@gmail.com>
2016-09-23 09:40:20 -0500

    Rename confusing variables to more semantic

--- app/components/datepicker/DatepickerDirective.js
+++ app/components/datepicker/DatepickerDirective.js
@@ -43,6 +43,12 @@
                     opened: false
                 };
 
+                /**
+                 * Set initial values for min and max dates in both of datepicker.
+                 */
+                vm.datepickerStartDate = vm.dateOptions.minDate;
+                vm.datepickerEndDate = vm.dateOptions.maxDate;
+
                 vm.onChangeDatepicker = onChangeDatepicker;
 
                 vm.showDatepickerInfo = showDatepickerInfo;
@@ -55,16 +61,6 @@
 
                 vm.slider = defaultSliderValue();
 
-                /**
-                 * Set initial values for min and max dates in both of datepicker.
-                 */
-                vm.setInitialDates = function(){
-                    vm.dts = vm.dateOptions.minDate;
-                    vm.dte = vm.dateOptions.maxDate;
-                };
-
-                vm.setInitialDates();
-
                 scope.$on('setHistogram', setHistogram);
 
                 scope.$on('slideEnded', slideEnded);
@@ -72,33 +68,34 @@
                 /**
                  * Will be called on click on start datepicker.
                  * `minDate` will be reset to the initial value (e.g. 2000-01-01),
-                 * `maxDate` will be adjusted with the `scope.dte` value to
+                 * `maxDate` will be adjusted with the `scope.datepickerEndDate` value to
                  *  restrict it not to be below the `minDate`.
                  */
                 function openStartDate() {
                     vm.startDate.opened = true;
                     vm.dateOptions.minDate = vm.initialDateOptions.minDate;
-                    vm.dateOptions.maxDate = vm.dte;
+                    vm.dateOptions.maxDate = vm.datepickerEndDate;
                 }
 
 
                 /**
                  * Will be called on click on end datepicker.
                  * `maxDate` will be reset to the initial value (e.g. 2016-12-31),
-                 * `minDate` will be adjusted with the `scope.dts` value to
+                 * `minDate` will be adjusted with the `scope.datepickerStartDate` value to
                  *  restrict it not to be bigger than the `maxDate`.
                  */
                 function openEndDate() {
                     vm.endDate.opened = true;
                     vm.dateOptions.maxDate = vm.initialDateOptions.maxDate;
-                    vm.dateOptions.minDate = vm.dts;
+                    vm.dateOptions.minDate = vm.datepickerStartDate;
                 }
 
                 /**
                  * Will be fired after the start and the end date was chosen.
                  */
                 function onChangeDatepicker(){
-                    vm.dateString = getFormattedDateString(vm.dts, vm.dte);
+                    vm.dateString = getFormattedDateString(vm.datepickerStartDate,
+                                                            vm.datepickerEndDate);
                     performDateSearch();
                 }
 
@@ -107,7 +104,7 @@
                       maxDate.toISOString().replace('.000Z','') + ']';
                 }
 
-                function stringToDate(dateString) {
+                function stringToStartEndDateArray(dateString) {
                     var dateArray = dateString.split(' TO ');
                     if (angular.isString(dateString) && dateArray.length === 2) {
                         dateArray[0] = new Date(dateArray[0].slice(1,11));
@@ -121,13 +118,14 @@
                 }
 
                 function onSubmitDateText() {
-                    var dateArray = stringToDate(vm.dateString);
+                    var dateArray = stringToStartEndDateArray(vm.dateString);
                     if (dateArray !== null) {
-                        vm.dts = dateArray[0];
-                        vm.dte = dateArray[1];
+                        vm.datepickerStartDate = dateArray[0];
+                        vm.datepickerEndDate = dateArray[1];
                         performDateSearch();
                     } else{
-                        vm.dateString = getFormattedDateString(vm.dts, vm.dte);
+                        vm.dateString = getFormattedDateString(vm.datepickerStartDate,
+                                                                vm.datepickerEndDate);
                     }
                 }
 
@@ -151,9 +149,10 @@
                 function slideEnded() {
                     var minKey = vm.slider.minValue;
                     var maxKey = vm.slider.maxValue;
-                    vm.dts = new Date(vm.slider.counts[minKey].value);
-                    vm.dte = new Date(vm.slider.counts[maxKey].value);
-                    vm.dateString = getFormattedDateString(vm.dts, vm.dte);
+                    vm.datepickerStartDate = new Date(vm.slider.counts[minKey].value);
+                    vm.datepickerEndDate = new Date(vm.slider.counts[maxKey].value);
+                    vm.dateString = getFormattedDateString(vm.datepickerStartDate,
+                                                            vm.datepickerEndDate);
                     performDateSearch();
                 }
 
