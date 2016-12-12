b619e9e504c5718c994bcf9bd7ff2d3266e455fc
Lenninlasd <lenninlasd@gmail.com>
2016-12-12 11:38:26 -0500

    Fix all test

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function ( config ) {
       'node_modules/angularjs-slider/dist/rzslider.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'node_modules/ol3-google-maps/dist/ol3gm.js',
+      'https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyAjtdt_Db2IOZDVYznVqot45xUs1tNhhDw',
       'app/**/*.js',
       'tests/**/*.spec.js',
       'app/**/*.tpl.html'
